7891daaf5c591f29c906d1157571aba38713222d
scott <scottjthayer@gmail.com>
2021-11-03 08:23:30 -0400

    cards API lab first commit

---
+++ DeckofCardsAPI Lab/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using DeckofCardsAPI_Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckofCardsAPI_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CardDAL cardDAL = new CardDAL();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DrawCards()
+        {
+
+            CardModel result = cardDAL.GetData();
+            if (result.remaining < 5)
+            {
+                cardDAL.GetShuffle();
+            }
+            return View(result);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckofCardsAPI Lab/Models/CardDAL.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DeckofCardsAPI_Lab.Models
+{
+    public class CardDAL
+    {
+        public void GetShuffle()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/0rfo1nerk97c/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+
+
+        public CardModel GetData()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/0rfo1nerk97c/draw/?count=5";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardModel result = JsonConvert.DeserializeObject<CardModel>(JSON);
+
+            return result;
+        }
+
+    }
+}
